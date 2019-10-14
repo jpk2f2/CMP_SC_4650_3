d970e2b6bf4e947e2edc6ae30817bdae33d82701
Jason Kayser <jpk2f2@mail.missouri.edu>
2019-10-14 09:59:20 -0500

    Added utility functions from previous project; read in images

---
+++ python/main.py
@@ -0,0 +1,13 @@
+import cv2
+import matplotlib
+
+matplotlib.use('TkAgg')
+
+im_1016 = cv2.imread('resources/Fig1016.tif', 0)
+im_1001a = cv2.imread('resources/Fig1001(a).tif', 0)
+im_1001b = cv2.imread('resources/Fig1001(b).tif', 0)
+im_1001c = cv2.imread('resources/Fig1001(c).tif', 0)
+im_1001d = cv2.imread('resources/Fig1001(d).tif', 0)
+im_1001e = cv2.imread('resources/Fig1001(e).tif', 0)
+im_1001f = cv2.imread('resources/Fig1001(f).tif', 0)
+
---
+++ python/utility.py
@@ -0,0 +1,72 @@
+import numpy as np
+import scipy.signal
+import cv2
+
+
+# preprocesses given image, returns padded version and array w/ original dimensions for final image
+# takes in image, desired padding, and pad type such as 'zero' or 'repeat'
+def prepare_image(im: np.ndarray, padding: int, pad_type: str):
+    dimensions = im.shape  # get dimensions of original image for creating new image
+    # convert brg to grayscale
+    if (len(dimensions)) > 2:
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    # create new image w/ appropriate dimensions
+    im2 = np.zeros((dimensions[0], dimensions[1]))
+
+    # use specified padding
+    if pad_type == 'zero':
+        # pad with zeros
+        im = np.pad(im, padding, 'constant', constant_values=0)
+    elif pad_type == 'repeat':
+        # copy nextdoor pixel for padding
+        im = np.pad(im, padding, 'symmetric')
+    else:
+        print('This should not have been reached')
+
+    return im.astype(dtype='float64'), im2.astype(dtype='float64')  # return processed image
+
+
+# post processes given image
+# takes in image and whether or not to convert to rgb
+def pp_image(im: np.ndarray, g2rgb: bool) -> np.ndarray:
+    if g2rgb:
+        im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
+    # return pp image with correct type
+    return im.astype(dtype='uint8')
+
+
+# creates a gaussian matrix using a given kernel size for convulation w/ an image
+# returns the guassian matrix
+# for example, a kernel size of 2 creates a 5x5 gaussian array
+def create_gauss_conv(kernel: int) -> np.ndarray:
+    # create array to hold guassian array
+    final = np.zeros(shape=(2 * kernel + 1, 2 * kernel + 1))
+    tot = 0
+    # loop through array, creating gaussian distribution
+    for i in range(-kernel, kernel + 1):
+        for j in range(-kernel, kernel + 1):
+            # create gaussian dividend
+            tmp = -1 * ((i ** 2 + j ** 2) / (2 * (kernel ** 2)))
+            # complete gaussian function and place it in dest
+            final[i + kernel, j + kernel] = math.exp(tmp) / (2 * np.pi * kernel ** 2)
+            # count total for normalization
+            tot = tot + final[i + kernel, j + kernel]
+
+    # normalize gaussian array
+    final = final / tot
+
+    return final
+
+
+def gauss_filter(im: np.ndarray, kernel: int) -> np.ndarray:
+    # check if premade desired
+    _filter = create_gauss_conv(kernel)  # create a guassian filter for the given kernel
+
+    im, im_proc = prepare_image(im, kernel, 'zero')  # preprocess image
+    # convolve image with filter
+    # 'valid' specified to use preprocessed padding
+    im_proc = scipy.signal.convolve2d(im, _filter, 'valid')
+
+    # post process image and return it
+    return pp_image(im_proc, False)
